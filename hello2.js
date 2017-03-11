8c142e94f8a1f075153abfcbe72f80fd9df57f60
lszita <lajos.szita@gmail.com>
2017-03-11 21:57:27 +0100

    mysql test

--- hello2.js
+++ hello2.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env nodejs
-var http = require('http');
+const http = require('http');
+const mysql = require('mysql');
+const connection = mysql.createConnection(process.env.DB_URL);
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from second app\n');
+
+  connection.query('SELECT NOW() AS THE_TIME', (err,dbres,fields) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World from second app the time from db is:' + dbres +  ' \n');
+    connection.end((err) => {
+      console.log('closed conn');
+    });
+  });
+  
 }).listen(8081, 'localhost');
 console.log('Server running at http://localhost:8081/');
