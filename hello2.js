cc6911399af53d6708243324e273d3a88270b379
lszita <lajos.szita@gmail.com>
2017-04-21 22:14:24 +0200

    fixed ws path

--- hello2.js
+++ hello2.js
@@ -2,7 +2,7 @@
 
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http, { path : '/node/v2/socket.io'} );
+var io = require('socket.io')(http, { path : '/node/v2/socket.io/'} );
 
 io.on('connection', function(socket){
   console.log('a user connected');
